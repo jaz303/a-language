08f1f8af957a068828e1bedf6f9a01db7b8532e6
Jason Frame <jason@onehackoranother.com>
2012-05-28 10:15:25 +0100

    refactor main executable, add syntax check option

--- src/menace.c
+++ src/menace.c
@@ -8,12 +8,69 @@
 #include "menace/parser.h"
 #include "menace/debug.h"
 
+typedef struct active_file {
+    FILE            *file;
+    context_t       context;
+    scanner_t       *scanner;
+    parser_t        parser;
+} active_file_t;
+
+/*
+ * Global State
+ */
+
+enum {
+    RUN,
+    HELP,
+    SYNTAX_CHECK,
+    PRETTY_PRINT
+};
+
+int mode    = RUN;
+int verbose = 0;
+
+static active_file_t* open_file(const char *filename) {
+    
+    active_file_t *out = malloc(sizeof(active_file_t));
+    if (out == NULL) {
+        fprintf(stderr, "couldn't allocate storage for active file\n");
+        exit(EX_UNAVAILABLE);
+    }
+    
+    out->file = fopen(filename, "r");
+    if (out->file == NULL) {
+        fprintf(stderr, "couldn't read file %s\n", filename);
+        exit(EX_NOINPUT);
+    }
+    
+    out->scanner = scanner_create_for_file(out->file);
+    if (out->scanner == NULL) {
+        fprintf(stderr, "couldn't create scanner\n");
+        exit(EX_UNAVAILABLE);
+    }
+    
+    if (!context_init(&out->context)) {
+        fprintf(stderr, "couldn't initialise context\n");
+        exit(EX_UNAVAILABLE);
+    }
+    
+    if (!parser_init(&out->parser, &out->context, out->scanner)) {
+        fprintf(stderr, "couldn't initialise parser\n");
+        exit(EX_UNAVAILABLE);
+    }
+    
+    return out;
+    
+}
+
 void display_usage(int extended) {
     printf( "Usage: menace [switches] [--] [programfile] [arguments]\n");
     
     if (extended) {
         printf( "  -h, --help       display this message\n"
+                "  -c               syntax check only\n"
                 "  --pretty-print   parse programfile and pretty print to stdout\n"
+                "  -v, --verbose    be verbose\n"
             );
     }
 }
@@ -25,55 +82,41 @@ void require_arg(int ix, int argc) {
     }
 }
 
-void init_context(context_t *ctx) {
-    if (!context_init(ctx)) {
-        fatal_error("coldn't initialise context");
-    }
-}
-
 void do_run(int ix, int argc, char *argv[]) {
     require_arg(ix, argc);
+    active_file_t *input = open_file(argv[ix]);
     
-    context_t ctx;
-    init_context(&ctx);
-    
+    fclose(input->file);
 }
 
 void do_pretty_print(int ix, int argc, char *argv[]) {
     require_arg(ix, argc);
+    active_file_t *input = open_file(argv[ix]);
     
-    FILE *file = fopen(argv[ix], "r");
-    if (file == NULL) {
-        printf("couldn't read file %s\n", argv[ix]);
-        exit(EX_NOINPUT);
-    }
-    
-    context_t ctx;
-    init_context(&ctx);
-    
-    scanner_t *scanner = scanner_create_for_file(file);
-    
-    parser_t parser;
-    parser_init(&parser, &ctx, scanner);
-    
-    ast_statements_t *stmts = parser_parse(&parser);
+    ast_statements_t *stmts = parser_parse(&input->parser);
     if (stmts == NULL) {
         printf("parse error\n");
         exit(EX_DATAERR);
     }
     
-    fclose(file);
+    fclose(input->file);
     
-    pretty_print(&ctx, stmts, stdout);
+    pretty_print(&input->context, stmts, stdout);
 }
 
-enum {
-    RUN             = 0,
-    HELP            = 1,
-    PRETTY_PRINT    = 2
-};
-
-int mode = RUN;
+void do_syntax_check(int ix, int argc, char *argv[]) {
+    require_arg(ix, argc);
+    active_file_t *input = open_file(argv[ix]);
+    
+    parser_parse(&input->parser);
+    fclose(input->file);
+    
+    if (input->parser.error) {
+        exit(EX_DATAERR);
+    } else {
+        printf("%s: no syntax errors detected\n", argv[ix]);
+    }
+}
 
 int main(int argc, char *argv[]) {
     
@@ -81,19 +124,26 @@ int main(int argc, char *argv[]) {
         
         int option_index;
         static struct option options[] = {
-            { "help",           0, &mode, HELP },
-            { "pretty-print",   0, &mode, PRETTY_PRINT },
+            { "help",           0, &mode,       HELP },
+            { "pretty-print",   0, &mode,       PRETTY_PRINT },
+            { "verbose",        0, &verbose,    1 },
             { 0, 0, 0, 0 }
         };
         
-        int c = getopt_long(argc, argv, "h", options, &option_index);
+        int c = getopt_long(argc, argv, "chv", options, &option_index);
         if (c == -1) {
             break;
         }
         
         switch (c) {
+            case 'c':
+                mode = SYNTAX_CHECK;
+                break;
             case 'h':
                 mode = HELP;
+                break;
+            case 'v':
+                verbose = 1;
                 break;
         }
         
@@ -103,8 +153,9 @@ int main(int argc, char *argv[]) {
         case HELP:              display_usage(1);                       break;
         case RUN:               do_run(optind, argc, argv);             break;
         case PRETTY_PRINT:      do_pretty_print(optind, argc, argv);    break;
+        case SYNTAX_CHECK:      do_syntax_check(optind, argc, argv);    break;
     }
     
-    return 0;
+    exit(0);
     
 }
