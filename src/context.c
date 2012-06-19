a0d981b39792f858142819f80b6c773cba6d1a75
Jason Frame <jason@onehackoranother.com>
2012-06-19 22:51:29 +0100

    native function allocated by GC

--- src/context.c
+++ src/context.c
@@ -1,18 +1,16 @@
 #include "menace/context.h"
 #include "menace/intern.h"
+#include "menace/gc.h"
 
 #include <stdlib.h>
 
 obj_native_function_t* ctx_register_native_function(context_t *ctx, const char *name, native_fn_f fn, void *userdata) {
     
-    obj_native_function_t *obj = malloc(sizeof(obj_native_function_t));
-    if (!fn) {
+    obj_native_function_t *obj = mnc_gc_alloc_native_function(ctx);
+    if (!obj) {
         return 0;
     }
     
-    // TODO: allocate from gc
-    
-    obj->obj.meta   = &NativeFunction;
     obj->name       = string_to_intern(ctx, name);
     obj->fn         = fn;
     obj->userdata   = userdata;
